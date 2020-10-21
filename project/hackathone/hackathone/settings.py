2f5627817b7a8c241c967195c5df28f6ce475ebf
glossyyoon <mypju@naver.com>
2020-10-22 01:45:55 +0900

    heroku

--- project/hackathone/hackathone/settings.py
+++ project/hackathone/hackathone/settings.py
@@ -136,8 +136,5 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
-
-""" import dj_database_url
-
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env) """
+DATABASES['default'].update(db_from_env)
